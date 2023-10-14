5b2c179471412daec69b814e95e39afe7b36800f
anoopkarnik <danoopkarnik@gmail.com>
2023-10-14 21:11:08 +0530

    update book details

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
 from flask import Flask
 import threading
 import os
-# from main.models.Model import db
 from main.controllers import Controller
-from config import LocalConfig,CloudConfig
+from dotenv import load_dotenv
+load_dotenv()
+
 def create_app():
 	app = Flask(__name__)
-	app.config["ENV"] = os.environ.get("APP_ENV", "local")
-	if app.config["ENV"] =="local":
-		app.config.from_object(LocalConfig)
-	elif app.config["ENV"] =="cloud":
-		app.config.from_object(CloudConfig)
-	# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://'+app.config['DB_USERNAME']+':'+app.config['DB_PASSWORD']+'@'+app.config['DB_HOST']+':'+app.config['DB_PORT']+'/'+app.config['DB_NAME']
-	# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-	# db.init_app(app)
-	# with app.app_context():
-	# 	db.create_all()
 	app.register_blueprint(Controller.payload_controller)
 	return app
 
--- main/controllers/Controller.py
+++ main/controllers/Controller.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, request, jsonify
+from ..services.updates import update_books
 
 payload_controller = Blueprint("payload_controller",__name__)
 
 @payload_controller.route("/",methods=["GET"])
 def health_check():
 	return jsonify({"status":"success"})
+
+@payload_controller.route("/update_books",methods=["POST"])
+def update_books_controller():
+	update_books()
+	return jsonify({'message':'Books updated'})
